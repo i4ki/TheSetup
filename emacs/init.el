08e50b6b42888a3bd80afcef06eaedc0fb1068cb
Tiago Natel de Moura <tiago4orion@gmail.com>
2015-03-15 17:01:33 -0300

    [CHANGE] improve compatibility using go-mode-autoloads

--- emacs/init.el
+++ emacs/init.el
@@ -43,7 +43,7 @@
 (global-set-key (kbd "C-c C-w") 'i3-manager-find-window)
 
 ;; Go
-(require 'go-mode-load)
+(require 'go-mode-autoloads)
 
 (add-hook 'go-mode-hook (lambda ()
 			  (add-hook 'before-save-hook 'gofmt-before-save)

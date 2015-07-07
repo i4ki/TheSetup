dcb375f29e5cb536151e85eb4623715c5327f053
Tiago Natel <tiago.natel@neoway.com.br>
2015-07-07 17:58:38 -0300

    [FEATURE] Added acme scripts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,9 @@
 
-
+set -x LANG pt_BR.UTF-8
 set -x GOROOT ~/projects/personal/go
 set -x GOPATH ~/projects/go-workspace
 set -x PLAN9 /usr/local/plan9
+set -x ACMEPATH ~/acme
 
-set -x PATH $PATH $GOROOT/bin $GOPATH/bin $PLAN9/bin
+set -x PATH $PATH $GOROOT/bin $GOPATH/bin $PLAN9/bin $ACMEPATH
 
